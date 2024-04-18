366305613fd2e6788f417440c84cf2cc64462893
andreiascpires <103081025+andreiascpires@users.noreply.github.com>
2024-04-18 22:01:59 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home() {
-    return  <h1>Herlon, eu amo você. Se você me ama, dá uma risadinha!!! S2 </h1>
-
+  return <h1>Herlon, eu amo você. Se você me ama, dá uma risadinha!!! S2 </h1>;
 }
 
 export default Home;
+
+function teswte() {
+  console.log("ai");
+}
